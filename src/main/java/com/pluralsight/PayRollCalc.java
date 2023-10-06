cc5092e26d8c08e16512bdb5f94e58ef493a59b7
ErinnS <esummers@appdev.yearup.org>
2023-10-06 15:24:40 -0400

    PayRollCalc

---
+++ src/main/java/com/pluralsight/PayRollCalc.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayRollCalc {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print(" Enter your name: ");
+        int name = myScanner.nextInt();
+
+        System.out.print(" Hours worked? ");
+        double num1 = myScanner.nextFloat();
+
+    }
+
+
+}
+
